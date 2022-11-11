eb31de21fd8ef137dc2ca4d1c4a85bcf603e79a0
LeChiLim <limlechi@gmail.com>
2022-11-11 18:35:59 +0800

    Second input

--- Q1/solution.cpp
+++ Q1/solution.cpp
@@ -1,13 +1,32 @@
 #include <iostream>
-#include <string.h>  
+#include <sstream>
+#include <string>
 #include <vector>
+#include <iterator>
 
 int main()
 {
     //get input
     int totalNumTshirts;
 
+    std::vector<std::string> TshirtsAvailable;
+
+    //number of t shirts
     std::cin >> totalNumTshirts;
+
+    //put t shirts available into a string vector
+    
+    std::string tshirtAvailableString;
+    
+    std::cin >> tshirtAvailableString;
+
+    std::stringstream TshirtAvailableSS(tshirtAvailableString);
+    std::string temp;
+    while (std::getline(TshirtAvailableSS, temp, ' ')){
+        TshirtsAvailable.push_back(temp);
+    }
+
+    
 
     return 0;
 }
